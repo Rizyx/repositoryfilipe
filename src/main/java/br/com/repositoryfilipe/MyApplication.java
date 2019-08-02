f4ecea645e08d1253627c13fb405b1eabd58ceac
Noturno <filipebcordeirobr@hotmail.com>
2019-08-02 20:59:54 -0300

    done

---
+++ src/main/java/br/com/repositoryfilipe/MyApplication.java
@@ -0,0 +1,12 @@
+package br.com.repositoryfilipe;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("Rest")
+public class MyApplication extends ResourceConfig {
+		public MyApplication() {
+			packages("br.com.repositoryfilipe.controller");
+		}
+}
