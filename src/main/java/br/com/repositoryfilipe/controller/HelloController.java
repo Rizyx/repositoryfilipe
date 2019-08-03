0928e4a326046a194ad3eb3292319e38c6b4b51b
Noturno <filipebcordeirobr@hotmail.com>
2019-08-02 22:01:48 -0300

    done

--- src/main/java/br/com/repositoryfilipe/controller/HelloController.java
+++ src/main/java/br/com/repositoryfilipe/controller/HelloController.java
@@ -1,5 +1,18 @@
 package br.com.repositoryfilipe.controller;
 
-public class HelloController {
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("Hello")
+public class HelloController {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMensagem(@QueryParam("usuario")String usuario) {
+		return "Bem vindo: " + usuario;
+	}
 }
